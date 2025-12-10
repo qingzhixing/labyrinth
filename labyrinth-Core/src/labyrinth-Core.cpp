9f16ebaa02860b469b784b2ed57a2b5362f27de5
qingzhixing <qzsqqmail@qq.com>
2025-12-10 19:14:38 +0800

    Core.cpp中添加Validate

--- labyrinth-Core/src/labyrinth-Core.cpp
+++ labyrinth-Core/src/labyrinth-Core.cpp
@@ -1,5 +1,6 @@
 #include <arg_parse.h>
 #include <debug_log.h>
+#include <arg_validate.h>
 
 int main(int argc, char *argv[])
 {
@@ -11,5 +12,13 @@ int main(int argc, char *argv[])
 		DebugLog(LogLevel::ERROR, error_code.toMessage());
 		return error_code.toInt();
 	}
+
+	error_code = ValidateParsedResult(parsed_result);
+	if (error_code != GameCoreErrorCode::SUCCESS)
+	{
+		DebugLog(LogLevel::ERROR, error_code.toMessage());
+		return error_code.toInt();
+	}
+
 	return 0;
 }
