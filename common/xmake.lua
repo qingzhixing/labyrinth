4e11676dfa6d8a3ef2a547dc8efda44f94761ce2
qingzhixing <qzsqqmail@qq.com>
2025-12-10 22:15:58 +0800

    删除common-test注释

--- common/xmake.lua
+++ common/xmake.lua
@@ -10,10 +10,9 @@ target("common")
 	-- Add the include directories
 	add_includedirs("./include",{public = true})
 
--- 添加独立的测试目标
 target("common-test")
 	set_kind("binary")
 	set_languages("c++17")
 	add_deps("debug_utils")
-	add_deps("common", "testkit")  -- 测试目标依赖common和testkit
-	add_files("./test/*.cpp")      -- 添加测试文件
+	add_deps("common", "testkit")
+	add_files("./test/*.cpp")
