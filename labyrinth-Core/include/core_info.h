927649e012bb7e181ac0c218bd1d1aacea0c8d9b
qingzhixing <qzsqqmail@qq.com>
2025-12-13 23:04:16 +0800

    内联core_info.h

--- labyrinth-Core/include/core_info.h
+++ labyrinth-Core/include/core_info.h
@@ -4,10 +4,46 @@
 
 #include <game_info.h>
 #include <string>
+#include <iostream>
 
 inline const std::string PROGRAM_NAME = "labyrinth-Core";
 
-void PrintUsage();
-void PrintVersion();
+inline void PrintVersion()
+{
+	std::cout << GAME_NAME_ASCII_ART << std::endl;
+	std::cout << GAME_NAME << " " << GAME_VERSION << std::endl;
+	std::cout << GAME_DESCRIPTION << std::endl;
+	std::cout << "Author: " << AUTHOR << " (" << AUTHOR_EMAIL << ")" << std::endl;
+}
+
+inline void PrintUsage()
+{
+	std::cout
+		<< "Usage: "
+		<< PROGRAM_NAME
+		<< " [-m|--map FILE] [--move DIRECTION] [-v|--version]"
+		<< std::endl;
+
+	std::cout << "Options:" << std::endl;
+	std::cout
+		<< "  -m, --map FILE    Specify the map file to use"
+		<< std::endl;
+
+	std::cout
+		<< "      --move DIRECTION (up, down, left, right)    "
+		<< "Specify the direction to move"
+		<< std::endl;
+
+	std::cout
+		<< "  -v, --version    Show the version information"
+		<< std::endl;
+
+	std::cout
+		<< "  -h, --help    Show this help message"
+		<< std::endl;
+
+	std::cout << "Tips:" << std::endl;
+	std::cout << "  1. '--move' '-m' must be used together" << std::endl;
+}
 
 #endif // GAME_CORE_INFO_H
