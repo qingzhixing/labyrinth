42bc7269b60e2373cc37eb5cac2bc9daac2b86c9
qingzhixing <qzsqqmail@qq.com>
2025-12-13 22:32:04 +0800

    提取CoordinateRecorder,内联MapValidator

---
+++ labyrinth-Core/include/coordinate_recorder.h
@@ -0,0 +1,99 @@
+#pragma once
+#ifndef COORDINATE_RECORDER_H
+#define COORDINATE_RECORDER_H
+
+#include <types/coordinate.h>
+#include <types/map_cell.h>
+#include <types/core_error_code.h>
+#include <debug_log.h>
+
+/**
+ * @brief 坐标记录器 - 负责记录玩家和目的地的坐标
+ */
+class CoordinateRecorder
+{
+private:
+	Coordinate player_coordinate = INVALID_COORDINATE;
+	Coordinate destination = INVALID_COORDINATE;
+
+	static bool IsPlayerCell(MapCellType cell_type)
+	{
+		return cell_type == MapCellType::PLAYER ||
+			   cell_type == MapCellType::PLAYER_AT_DESTINATION;
+	}
+
+	static bool IsDestinationCell(MapCellType cell_type)
+	{
+		return cell_type == MapCellType::DESTINATION ||
+			   cell_type == MapCellType::PLAYER_AT_DESTINATION;
+	}
+
+public:
+	GameCoreErrorCode
+	RecordPlayerIfNeeded(MapCellType cell_type, int line, int column)
+	{
+		if (!IsPlayerCell(cell_type))
+		{
+			return GameCoreErrorCode::SUCCESS;
+		}
+
+		if (player_coordinate.IsValid())
+		{
+			DebugLog(LogLevel::ERROR,
+					 "Invalid map format: multiple players '0'/'W' found");
+			return GameCoreErrorCode::MAP_MULTIPLE_PLAYER;
+		}
+		player_coordinate = Coordinate(line, column);
+		DebugLog(LogLevel::DEBUG,
+				 "Player coordinate: %d, %d", line, column);
+
+		return GameCoreErrorCode::SUCCESS;
+	}
+
+	GameCoreErrorCode
+	RecordDestinationIfNeeded(MapCellType cell_type, int line, int column)
+	{
+		if (!IsDestinationCell(cell_type))
+		{
+			return GameCoreErrorCode::SUCCESS;
+		}
+		if (destination.IsValid())
+		{
+			DebugLog(LogLevel::ERROR,
+					 "Invalid map format: multiple destinations '@'/'W' found");
+			return GameCoreErrorCode::MAP_MULTIPLE_DESTINATION;
+		}
+		destination = Coordinate(line, column);
+		DebugLog(LogLevel::DEBUG,
+				 "Destination coordinate: %d, %d",
+				 line, column);
+		return GameCoreErrorCode::SUCCESS;
+	}
+
+	GameCoreErrorCode
+	RecordCoordinates(MapCellType cell_type, int line, int column)
+	{
+		auto player_error = RecordPlayerIfNeeded(cell_type, line, column);
+		if (player_error != GameCoreErrorCode::SUCCESS)
+		{
+			return player_error;
+		}
+
+		auto destination_error =
+			RecordDestinationIfNeeded(cell_type, line, column);
+		if (destination_error != GameCoreErrorCode::SUCCESS)
+		{
+			return destination_error;
+		}
+
+		return GameCoreErrorCode::SUCCESS;
+	}
+
+	Coordinate GetPlayerCoordinate() const { return player_coordinate; }
+	Coordinate GetDestination() const { return destination; }
+
+	bool HasPlayer() const { return player_coordinate.IsValid(); }
+	bool HasDestination() const { return destination.IsValid(); }
+};
+
+#endif // COORDINATE_RECORDER_H
--- labyrinth-Core/include/map_validator.h
+++ labyrinth-Core/include/map_validator.h
@@ -4,6 +4,8 @@
 
 #include <types/map_cell.h>
 #include <types/coordinate.h>
+#include <types/map_size.h>
+#include <debug_log.h>
 
 /**
  * @brief 地图验证器 - 负责地图格式和内容的验证
@@ -11,18 +13,79 @@
 class MapValidator
 {
 public:
-	static bool ValidateSize(int lines, int columns);
+	static bool ValidateSize(int lines, int columns)
+	{
+		if (lines > MAX_MAP_SIZE.lines)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Map too large: exceeds maximum lines %d",
+				MAX_MAP_SIZE.lines);
+			return false;
+		}
+		if (columns > MAX_MAP_SIZE.columns)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Map too large: exceeds maximum columns %d",
+				MAX_MAP_SIZE.columns);
+			return false;
+		}
+		return true;
+	}
 
 	static bool ValidateLineLengths(
 		int expected_length,
 		int actual_length,
-		int line_index);
+		int line_index)
+	{
+		if (expected_length != 0 && expected_length != actual_length)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Invalid map format: inconsistent line lengths at line %d",
+				line_index);
+			return false;
+		}
+		return true;
+	}
 
-	static bool ValidateCellType(MapCellType cell_type, char ch, int line_index);
+	static bool ValidateCellType(MapCellType cell_type, char ch, int line_index)
+	{
+		if (cell_type == MapCellType::INVALID)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Invalid map format: unknown cell type '%c' at line %d",
+				ch, line_index);
+			return false;
+		}
+		return true;
+	}
 
-	static bool ValidateMapNotEmpty(int total_lines);
+	static bool ValidateMapNotEmpty(int total_lines)
+	{
+		if (total_lines == 0)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Invalid map format: empty map file");
+			return false;
+		}
+		return true;
+	}
 
-	static bool ValidateHasDestination(const Coordinate &destination);
+	static bool ValidateHasDestination(const Coordinate &destination)
+	{
+		if (destination == INVALID_COORDINATE)
+		{
+			DebugLog(
+				LogLevel::ERROR,
+				"Invalid map format: no destination '@' found");
+			return false;
+		}
+		return true;
+	}
 };
 
 #endif // MAP_VALIDATOR_H
