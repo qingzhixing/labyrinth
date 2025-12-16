5569ab67451c97eee467e4e62578c3a333c7e685
qingzhixing <qzsqqmail@qq.com>
2025-12-16 23:48:16 +0800

    SystemTest好像没法运行,何意味?

--- labyrinth-Core/test/test_labyrinth-Core.cpp
+++ labyrinth-Core/test/test_labyrinth-Core.cpp
@@ -1,1 +1,38 @@
 #include <testkit.h>
+#include <types/core_error_code.h>
+
+SystemTest(
+	Labyrinth_Core_Help,
+	((const char *[]){"--help"}))
+{
+	assert(
+		result->exit_status ==
+		GameCoreErrorCode::HELP_REQUESTED);
+}
+
+SystemTest(
+	Labyrinth_Core_Help_Short,
+	((const char *[]){"-h"}))
+{
+	assert(
+		result->exit_status ==
+		GameCoreErrorCode::HELP_REQUESTED);
+}
+
+SystemTest(
+	Labyrinth_Core_Version,
+	((const char *[]){"--version"}))
+{
+	assert(
+		result->exit_status ==
+		GameCoreErrorCode::VERSION_REQUESTED);
+}
+
+SystemTest(
+	Labyrinth_Core_Version_Short,
+	((const char *[]){"-v"}))
+{
+	assert(
+		result->exit_status ==
+		GameCoreErrorCode::VERSION_REQUESTED);
+}
