638a522d80f9e52964eb7501ebc055f92b63f91f
qingzhixing <qzsqqmail@qq.com>
2025-12-18 20:59:56 +0800

    重构GameController类，添加公共方法和私有成员变量

--- labyrinth-UI/include/game_controller.h
+++ labyrinth-UI/include/game_controller.h
@@ -2,13 +2,24 @@
 #ifndef GAME_CONTROLLER_H
 #define GAME_CONTROLLER_H
 
+#include <string>
+#include <types/direction.h>
+#include <types/error_code.h>
+
 class GameController
 {
 public:
 	GameController();
 	~GameController();
 
-	
+public:
+	ErrorCode MovePlayer(Direction direction) const;
+	bool IsGameEnd() const;
+	ErrorCode PrintMap() const;
+
+private:
+	std::string core_executable_path;
+	std::string map_file_path;
 };
 
 #endif // GAME_CONTROLLER_H
